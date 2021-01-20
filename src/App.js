9d95f339aa529d88fd4f0c9efc1c7b7f0d555a13
Ana de Oliveira Rodrigues <ana.oliveira.rodrigues@gmail.com>
2021-01-20 14:06:46 -0500

    first changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav>
+          <ul>
+            <li>About</li>
+            <li><img src={logo} className="App-logo" alt="logo" />
+            </li>
+          </ul>
+        </nav>
+  </header>
+      
+      <main className="mainContent">
+
+        <section className="forms">
+          Forms
+         </section>
+        
+    </main>
+
     </div>
   );
 }
