9b96cc8d751111ee4e5b50e3a213d46992fb887b
Ana de Oliveira Rodrigues <ana.oliveira.rodrigues@gmail.com>
2021-01-20 16:22:15 -0500

    formLabel included

--- src/Form.js
+++ src/Form.js
@@ -1,28 +1,39 @@
 //Form.js
+import React, { Component } from 'react';
 import allForms from './allForms.js';
 
-const Form = () => {
-    return (
-        <section className="forms wrapper">
-            {allForms.map((form) => {
-                return (
-                    <form>
-                        <p><label for={form.label} className="formLabel">{form.name}</label>
-                        </p>
-                        {form.options.map((option) => {
-                            return (
-                                <fragment>
-                                    <input type="radio" name="" value={option.value} />
-                                    <label for={option.value}>{option.name}</label><br></br>
-                                </fragment>
-                            )
-                        })
-                        }
-                    </form>
-                )
-            })}
-        </section>
-    );
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            formLabel: "",
+        };
+    }
+    render() {
+        
+        return (
+            <section className="forms wrapper">
+                {allForms.map((form) => {
+                    this.state.formLabel = form.label;
+                    return (
+                        <form>
+                            <p><label for={form.label} className="formLabel">{form.name}</label>
+                            </p>
+                            {form.options.map((option) => {
+                                return (
+                                    <fragment>
+                                        <input type="radio" name={this.state.formLabel} value={option.value} />
+                                        <label for={option.value}>{option.name}</label><br></br>
+                                    </fragment>
+                                )
+                            })
+                            }
+                        </form>
+                    )
+                })}
+            </section>
+        );
+    }
 }
 
 export default Form;
